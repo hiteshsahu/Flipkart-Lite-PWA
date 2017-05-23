f871e1c02364c11f880c23e1e606e846d35beeef
LAPTOP-QQT7CRS9\User <shashank.deshmukh@qantasloyalty.com>
2017-05-23 16:01:49 +1000

    test with source tree

--- ecart-pwa/components/header/index.js
+++ ecart-pwa/components/header/index.js
@@ -8,7 +8,7 @@ export default class Header extends Component {
 			<header class={style.header}>
 				<h1>Preact App</h1>
 				<nav>
-					<Link activeClassName="active" href="/">Homeeee</Link>
+					<Link activeClassName="active" href="/">Home</Link>
 					<Link activeClassName="active" href="/profile">Me</Link>
 					<Link activeClassName="active" href="/profile/john">John</Link>
 				</nav>
